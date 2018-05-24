b6ab6f714c4cc2648fd0df346ac30445b487b480
Olivier Truong <oliv.truong@gmail.com>
2018-05-24 21:28:17 +0200

    using custom command

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -35,7 +35,13 @@ module.exports = function (config) {
                colors: true,
                logLevel: config.LOG_INFO,
                autoWatch: true,
-               browsers: ['Chrome','PhantomJS2'],
-               singleRun: false
+               browsers: [ 'ChromeHeadlessNoSandbox'],
+               singleRun: false,
+               customLaunchers: {
+                 ChromeHeadlessNoSandbox: {
+                   base: 'ChromeHeadless',
+                   flags: ['--no-sandbox']
+                 }
+               }
              });
 };
