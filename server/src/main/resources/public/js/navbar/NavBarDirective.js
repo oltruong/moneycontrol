971dabf207b1c5ff5cf64f00fb2f97dee414fa4a
Olivier Truong <oliv.truong@gmail.com>
2018-06-11 21:05:56 +0200

    cleanup

--- server/src/main/resources/public/js/navbar/NavBarDirective.js
+++
@@ -1,7 +0,0 @@
-moneyControlApp.directive('navbar', function () {
-    return {
-        controller: 'NavBarController',
-        templateUrl: "partials/directives/navbar.html",
-        restrict: "E"
-    };
-});
