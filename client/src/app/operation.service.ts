c58e49bd6de3dd3e4de145c93f755d4c75118051
Olivier Truong <olivier@nadeva.fr>
2019-05-08 14:55:45 +0200

    fix redirect

--- client/src/app/operation.service.ts
+++ client/src/app/operation.service.ts
@@ -3,28 +3,29 @@ import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {OperationModel} from "./models/operation.model";
 import {environment} from '../environments/environment';
+import {Router} from "@angular/router";
 
 @Injectable()
 export class OperationService {
-  private url: string;
+    private url: string;
 
-  constructor(private http: HttpClient) {
-    this.url = environment.backendUrl + '/operations';
-  }
+    constructor(private http: HttpClient, private router: Router) {
+        this.url = environment.backendUrl + '/operations';
+    }
 
-  list(params): Observable<Array<OperationModel>> {
-    return this.http.get<Array<OperationModel>>(this.url, {params});
-  }
+    list(params): Observable<Array<OperationModel>> {
+        return this.http.get<Array<OperationModel>>(this.url, {params});
+    }
 
-  update(operation: OperationModel) {
-    this.http.put(this.url + "/" + operation.id, operation).subscribe((response: HttpResponse<Object>) => {
-      console.log("retour " + response);
-    });
-  }
+    update(operation: OperationModel) {
+        this.http.put(this.url + "/" + operation.id, operation).subscribe((response: HttpResponse<Object>) => {
+            console.log("retour " + response);
+        });
+    }
 
-  refresh() {
-    this.http.post(this.url + "/refresh", {}).subscribe((response: HttpResponse<Object>) => {
-      console.log("retour " + response);
-    });
-  }
+    refresh() {
+        this.http.post(this.url + "/refresh", {}).subscribe((response: HttpResponse<Object>) => {
+            this.router.navigate(['/operations'], {queryParams: {category: 'empty'}});
+        });
+    }
 }
