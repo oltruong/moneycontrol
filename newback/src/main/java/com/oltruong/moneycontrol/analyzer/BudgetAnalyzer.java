52b63c710667202b58cb6b502f060fd7c3774f16
Olivier Truong <olivier@nadeva.fr>
2020-05-08 18:29:42 +0200

    cleanup

--- newback/src/main/java/com/oltruong/moneycontrol/analyzer/BudgetAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package com.oltruong.moneycontrol.analyzer;
-
-import com.oltruong.moneycontrol.operation.Operation;
-import com.oltruong.moneycontrol.rule.Rule;
-
-public interface BudgetAnalyzer {
-
-    Operation analyzeOperation(Operation operation, Iterable<Rule> rules);
-}
