f6c0b8fce5f865d161e0aa3ef6726c5f2342a6fe
Olivier Truong <olivier@nadeva.fr>
2020-06-10 23:13:00 +0200

    WIP

--- front/src/views/Statistics.vue
+++ front/src/views/Statistics.vue
@@ -3,11 +3,10 @@
         <div class="col-12">
             <h1>Statistics
             </h1>
-            <div>
-                <p v-for="statistic of statistics">
-                    {{statistic}}
-                </p>
-            </div>
+        </div>
+
+        <div class="col-12">
+            <highcharts class="chart" :options="chartOptions" :updateArgs="updateArgs"></highcharts>
         </div>
 
     </div>
@@ -16,11 +15,31 @@
 <script>
     import axios from 'axios';
 
+    Array.prototype.groupBy = function (prop) {
+        return this.reduce(function (groups, item) {
+            const val = item[prop]
+            groups[val] = groups[val] || []
+            groups[val].push(item)
+            return groups
+        }, {})
+    }
+
     export default {
         name: "statistics",
         data: function () {
             return {
                 statistics: [],
+                statistics2: [],
+                updateArgs: [true, true, {duration: 1000}],
+                chartOptions: {
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: 'Catégories'
+                    },
+                    series: []
+                }
             }
         },
         mounted() {
@@ -31,7 +50,44 @@
                 axios
                     .get(process.env.VUE_APP_BACKOFFICE_URL
                          + "rest/statistics")
-                    .then(response => (this.statistics = response.data));
+                    .then(response => {
+                        this.statistics = response.data;
+                        this.statistics2 = this.statistics.groupBy('category');
+
+                        console.info(this.statistics);
+
+                        for (let category in this.statistics2) {
+                            let serie = {
+                                name: category,
+                                color: '#6fcd98',
+                            }
+                            console.info(category)
+                            var points = this.statistics2[category]
+                            points.sort(function (a, b) {
+                                return a.month - b.month
+                            });
+                            console.log("AAA");
+                            console.log(points);
+                            let data = [];
+                            let total = 0;
+                            for (let i in points) {
+                                const point = points[i];
+                                // console.info("BB");
+                                // console.info(point["amount"] + " / " + point["month"]);
+                                data.push([point["month"], -point["amount"]]);
+                                total += point["amount"];
+                            }
+                            console.info("TOOOTAL " + total + " " + category);
+                            if (category!="")
+                            serie.data = data;
+                            this.chartOptions.series.push(serie);
+
+                            if (total < 0) {
+                                console.warn("HEYYY " + total + " " + category);
+                            }
+
+                        }
+                    });
             }
 
         }
