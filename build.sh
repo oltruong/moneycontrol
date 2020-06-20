3d75e02e8b21308f1377adf67c46ae0a0ebe7757
Olivier Truong <olivier@nadeva.fr>
2020-06-20 16:07:16 +0200

    Prep for build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@
 rm -rf back/src/main/resources/public/*
 cp -R front/dist/ back/src/main/resources/META-INF/resources/
 
-(cd back && ./gradlew quarkusBuild --uber-jar --scan)
+(cd back && ./gradlew quarkusBuild --uber-jar)
